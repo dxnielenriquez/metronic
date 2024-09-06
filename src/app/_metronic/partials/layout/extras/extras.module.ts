7ce161cd9fee6fdc046f4465ebd7fa60f9d77716
Daniel Enriquez <daniel.enriquezmontalvo@gmail.com>
2024-09-06 11:20:41 -0700

    quick links

--- src/app/_metronic/partials/layout/extras/extras.module.ts
+++ src/app/_metronic/partials/layout/extras/extras.module.ts
@@ -3,7 +3,6 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {InlineSVGModule} from 'ng-inline-svg-2';
 import {NotificationsInnerComponent} from './dropdown-inner/notifications-inner/notifications-inner.component';
-import {QuickLinksInnerComponent} from './dropdown-inner/quick-links-inner/quick-links-inner.component';
 import {UserInnerComponent} from './dropdown-inner/user-inner/user-inner.component';
 import {LayoutScrollTopComponent} from './scroll-top/scroll-top.component';
 import {TranslationModule} from '../../../../modules/i18n';
@@ -14,7 +13,6 @@ import { SharedModule } from "../../../shared/shared.module";
 @NgModule({
   declarations: [
     NotificationsInnerComponent,
-    QuickLinksInnerComponent,
     UserInnerComponent,
     LayoutScrollTopComponent,
   ],
@@ -29,7 +27,6 @@ import { SharedModule } from "../../../shared/shared.module";
   ],
   exports: [
     NotificationsInnerComponent,
-    QuickLinksInnerComponent,
     UserInnerComponent,
     LayoutScrollTopComponent,
   ],
