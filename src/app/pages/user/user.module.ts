47ce2f238cdf915b77e5dabb48e6469bb7bc3d17
Daniel Enriquez <daniel.enriquezmontalvo@gmail.com>
2024-09-26 14:00:04 -0700

    eliminado

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -1,23 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {UserListingComponent} from './user-listing/user-listing.component';
 import {RouterLink, RouterModule, Routes} from '@angular/router';
 import {SharedModule} from 'src/app/_metronic/shared/shared.module';
 import {NgbCollapseModule, NgbDropdownModule, NgbNavModule, NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SweetAlert2Module} from '@sweetalert2/ngx-sweetalert2';
-import {
-  MatCell, MatCellDef,
-  MatColumnDef,
-  MatHeaderCell, MatHeaderCellDef,
-  MatHeaderRow,
-  MatRow,
-  MatTableModule
-} from "@angular/material/table";
-import {MatIcon} from "@angular/material/icon";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatSort} from "@angular/material/sort";
-import {MatInput, MatInputModule} from "@angular/material/input";
 import {CrudModule} from "../crudejemplo/crud/crud.module";
 import {CrudEditComponent} from "../crudejemplo/crud-edit/crud-edit.component";
 import {ShareModule} from "../../share/share.module";
@@ -39,7 +26,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [UserListingComponent,],
+  declarations: [],
   imports: [
     CommonModule,
     FormsModule,
